fb320232bc2660062844f707237c481054b2ab7f
kamaljeet <kamaljeet8130@gmail.com>
2025-09-04 00:33:42 +0530

    added locators for Categories sections

--- src/main/java/pages/home/CategoriesSection.java
+++ src/main/java/pages/home/CategoriesSection.java
@@ -1,4 +1,18 @@
 package pages.home;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class CategoriesSection {
+    private WebDriver driver;
+
+    //locators:
+    private By categoriesSliding = By.xpath("(//div[contains(@class,'mySwiper')])[1]//div[contains(@class,'swiper-slide')]");
+    private By activeCategory = By.xpath("(//div[contains(@class,'mySwiper')])[1]//div[contains(@class,'swiper-slide-active')]");
+    private By leftArrow= By.xpath("(//h1[text()='Browse By Categories']/following-sibling::div//*[name()='svg'])[1]");
+    private By rightArrow= By.xpath("(//h1[text()='Browse By Categories']/following-sibling::div//*[name()='svg'])[2]");
+    private By viewAll = By.xpath("//h1[text()='Browse By Categories']/ancestor::div[contains(@class,'relative')]//a[@href='categories']");
+
+
+
 }
