287b9924eb169a7224794c59f9acff1fd9b87293
kamaljeet <kamaljeet8130@gmail.com>
2025-09-11 18:51:44 +0530

    added product card component

---
+++ src/main/java/pages/components/ProductCard.java
@@ -0,0 +1,73 @@
+package pages.components;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.lang.reflect.Executable;
+import java.util.List;
+
+public class ProductCard {
+   private WebElement rootElement;
+
+   public ProductCard(WebElement element){
+       this.rootElement =rootElement;
+   }
+   private By productName =By.xpath(".//div[contains(@data-testid,'book-title')]");
+   private By productImage = By.xpath(".//div[contains(@data-testid,'product-image')]/img");
+   private By productRating = By.xpath(".//div[contains(@data-testid,'book-rating')]");
+   private By oldPrice = By.xpath(".//p[cotains(@data-testid,'actualPrice')]");
+   private By discountedPrice = By.xpath(".//p[contains(@data-testid,'discounted-price')]");
+   private By savedPrice = By.xpath(".//p[contains(@data-testid,'save-price')]");
+   private By wishlistIcon = By.xpath(".//div[contains(@data-testid,'wishlist-icon')]");
+   private By addToCartIcon = By.xpath(".//div[contains(@data-testid,'add-to-cart')]");
+   private By viewDetailButton = By.xpath(".//div[contains(@data-testid,'view-detail-button')]");
+
+   public String getName(){
+       return rootElement.findElement(productName).getText().trim();
+   }
+   public String getImageUrl(){
+       return rootElement.findElement(productImage).getAttribute("src");
+   }
+   public String getOldPrice(){
+       try {
+           return rootElement.findElement(oldPrice).getText().trim();
+       }catch (Exception e){
+           return "";
+       }
+   }
+   public String getDiscountedPrice(){
+       try{
+           return rootElement.findElement(discountedPrice).getText().trim();
+       }catch (Exception e){
+           return "";
+       }
+   }
+   public String getSavedPrice(){
+       try {
+           return rootElement.findElement(savedPrice).getText().trim();
+       }catch (Exception e){
+           return "";
+       }
+   }
+   public int getProductRating(){
+       try {
+           WebElement ratingDiv = rootElement.findElement(productRating);
+           List<WebElement> svgs = ratingDiv.findElements(By.xpath(".//*[name()='svg']"));
+           return svgs.size();
+       }catch (Exception e){
+           return -1;
+       }
+   }
+   public void clickAddToCartIcon(){
+       rootElement.findElement(addToCartIcon).click();
+   }
+   public void clickWishlistIcon(){
+       rootElement.findElement(wishlistIcon).click();
+   }
+   public void clickViewDetailsButton(){
+       rootElement.findElement(viewDetailButton).click();
+   }
+
+
+}
