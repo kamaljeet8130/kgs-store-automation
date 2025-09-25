b6f56d5fec5793e7e73afbf254cbd3f5c6bda198
kamaljeet <kamaljeet8130@gmail.com>
2025-09-26 00:23:21 +0530

    updated locator and methods

--- src/main/java/pages/home/ProductListingSection.java
+++ src/main/java/pages/home/ProductListingSection.java
@@ -3,8 +3,12 @@ package pages.home;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.devtools.v137.network.model.DataReceived;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.components.ProductCard;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,14 +22,17 @@ public class ProductListingSection {
     private By allSections = By.xpath("//div[contains(@data-testid,'product-section')]");
     private By sectionTitles = By.xpath(".//h1");
     private By productCards = By.xpath(".//div[contains(@class,'swiper-slide')]");
-    private By viewAllLink = By.xpath("");
+    private By viewAllLink = By.xpath("//div[contains(@data-testid,'product-section')]//div/a[text()='View All']");
 
     public List<String> getAllSectionTitles() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         List<WebElement> sections = driver.findElements(allSections);
         List<String> titles = new ArrayList<>();
         for (WebElement section : sections) {
             try {
-                WebElement header = section.findElement(By.xpath(".//h1"));
+                WebElement header = wait.until(
+                        ExpectedConditions.presenceOfNestedElementLocatedBy(section,sectionTitles)
+                );
                 titles.add(header.getText().trim());
             } catch (Exception e) {
                 titles.add("UNKNOWN");
@@ -35,7 +42,7 @@ public class ProductListingSection {
     }
 
     private WebElement getSectionByTitle(String sectionTitle) {
-        String path = " //div[contains(@data-testid,'product-section')]//h1[normalize-space()='" + sectionTitle + "']/ancestor::div[contains(@data-testid,'product-section')]";
+        String path = "//h1[normalize-space()='" + sectionTitle + "']/ancestor::div[contains(@data-testid,'product-section')]";
         return driver.findElement(By.xpath(path));
     }
 
