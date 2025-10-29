41b0db5c1bbb5ff948235428c60f338824034ece
kamaljeet <kamaljeet8130@gmail.com>
2025-10-29 18:22:45 +0530

    added test cases for banner section and report logger

--- src/test/java/tests/home/HomePageTests.java
+++ src/test/java/tests/home/HomePageTests.java
@@ -2,7 +2,9 @@ package tests.home;
 
 import base.BaseTest;
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import pages.home.BannerSection;
 import pages.home.HomePage;
 
@@ -16,27 +18,39 @@ public class HomePageTests extends BaseTest {
         HomePage home = new HomePage(driver);
         String currentUrl = home.getCurrentUrl();
         Assert.assertTrue(currentUrl.contains("store.kgs.ink"),"Home page Url Mismatch, Actual Url " + currentUrl);
+        Reporter.log("Store Url : " + currentUrl,true);
     }
-    @Test(description = "Verify all banners and images are displayed on the homepage")
-    public void verifyBannerIsDisplayed(){
+    @Test(description = "Verify atleast one banner is displayed on the homepage")
+    public void verifyBannerCountIsPositive(){
         HomePage home = new HomePage(driver);
         BannerSection banner = home.getBannerSection();
         int bannerCount = banner.getBannerCount();
-        Assert.assertTrue(bannerCount>0,"No Banner is displayed on Home Page!" + bannerCount);
-        System.out.println(bannerCount);
-
-//        boolean isBannerImageVisible = banner.areAllBannerImageDisplayed();
-//        Assert.assertTrue(isBannerImageVisible,"Some Banner Image are not Visible!");
-        for (String s : banner.getAllBannerRedirectUrls()){
-            System.out.println(s);
+        Assert.assertTrue(bannerCount>0,"No banner is Displayed on Home Page!" + bannerCount);
+        Reporter.log("Banner Count : " + bannerCount,true );
+    }
+    @Test(description = "Verify all banner images are displayed on the homepage!")
+    public void verifyAllBannerImagesAreDisplayed(){
+        HomePage home = new HomePage(driver);
+        BannerSection banner = home.getBannerSection();
+        SoftAssert softAssert = new SoftAssert();
+        boolean isBannerImageDisplayed = banner.areAllBannerImageDisplayed();
+        softAssert.assertTrue(isBannerImageDisplayed,"Some Banner Images are not displayed");
+        Reporter.log("Banner Image Src " ,true);
+        List<String> imagesSrc = new ArrayList<>();
+        for (String imageSrc : banner.getAllBannerImageSrc()){
+            Reporter.log(imageSrc,true);
+        }
+        softAssert.assertAll();
+    }
+    @Test(description = "Log all banner images URls displayed on the Home page")
+    public void verifyAllBannerImagesUrlsAreLoggedCorrectly(){
+        HomePage home = new HomePage(driver);
+        BannerSection banner = home.getBannerSection();
+        List<String> bannerUrls = banner.getAllBannerRedirectUrls();
+        Reporter.log("===Banner Images Url===" , true);
+        for (String url : bannerUrls){
+            Reporter.log(url,true);
         }
 
-
-
-
     }
-
-
-
-
 }
