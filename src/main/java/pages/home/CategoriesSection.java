2262929aa4a5059fd62823ec63c9d0f767fef321
kamaljeet <kamaljeet8130@gmail.com>
2025-11-03 18:32:53 +0530

    implemented waits locators and actions

--- src/main/java/pages/home/CategoriesSection.java
+++ src/main/java/pages/home/CategoriesSection.java
@@ -1,9 +1,15 @@
 package pages.home;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utils.ScrollUtil;
 
+import java.time.Duration;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -11,9 +17,11 @@ import java.util.Set;
 
 public class CategoriesSection {
     private WebDriver driver;
+    private WebDriverWait wait;
 
     public CategoriesSection(WebDriver driver){
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
     //locators:
     private By categorySlideItems = By.xpath("(//div[contains(@class,'mySwiper')])[2]//div[contains(@class,'swiper-slide')]");
@@ -21,15 +29,26 @@ public class CategoriesSection {
     private By leftArrow= By.xpath("(//h1[text()='Browse By Categories']/following-sibling::div//*[name()='svg'])[1]");
     private By rightArrow= By.xpath("(//h1[text()='Browse By Categories']/following-sibling::div//*[name()='svg'])[2]");
     private By viewAll = By.xpath("//h1[text()='Browse By Categories']/ancestor::div[contains(@class,'relative')]//a[@href='categories']");
-    private By categoryName = By.xpath("(//div[contains(@class,'mySwiper')])[2]//div[contains(@class,'swiper-slide')]/p");
+    private By categoryName = By.xpath("//div[@data-testid='categories-slider']//div[@data-testid='category-card']//p");
+    private By categoryCard = By.xpath("//div[@data-testid='category-card']");
+    private By categoriesSlider = By.xpath("//div[@data-testid='categories-slider']");
 
+    public void scrollToCategorySection(){
+        ScrollUtil scrollUtil = new ScrollUtil(driver);
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(categoriesSlider));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(categoriesSlider));
+        scrollUtil.scrollToElement(driver.findElement(categoriesSlider));
+    }
     public int getCategoriesCount(){
-        return driver.findElements(categorySlideItems).size();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(categoriesSlider));
+              return driver.findElements(categorySlideItems).size();
     }
     public String getActiveCategoryName(){
         return driver.findElement(activeCategory).getText();
     }
     public void clickRightArrow(){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(categoriesSlider));
+        wait.until(ExpectedConditions.elementToBeClickable(rightArrow));
         driver.findElement(rightArrow).click();
     }
     public void clickLeftArrow(){
@@ -73,5 +92,7 @@ public class CategoriesSection {
     public boolean hasExpectedCategoryCount(int expectedCount){
         return getCategoriesCount()==expectedCount;
     }
-
+    public void clickCategoryCard(){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(categoryCard)).click();
+    }
 }
