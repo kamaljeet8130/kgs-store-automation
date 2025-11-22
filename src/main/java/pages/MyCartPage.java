8edea191390353cf685b348c72caa996f3c0e639
kamaljeet8130 <kamaljeet8130@gmail.com>
2025-11-22 18:31:08 +0530

    locator for checkout summary section of my cart

--- src/main/java/pages/MyCartPage.java
+++ src/main/java/pages/MyCartPage.java
@@ -10,10 +10,11 @@ public class MyCartPage {
     private WebDriver driver;
     private WebDriverWait wait;
 
-    public MyCartPage(WebDriver driver){
+    public MyCartPage(WebDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
+
     private By itemCart = By.xpath("//*[@data-testid='item-card']");
     private By emptyCartImage = By.xpath(".//*div/img[@alt='No Product in Cart']");
     private By emptyCartMessage = By.xpath(".//*div/h1[text()='Cart Empty']");
@@ -25,9 +26,15 @@ public class MyCartPage {
     private By cartImage = By.xpath(".//*div[@data-testid='cart-image']/img");
     private By itemDeleteButton = By.xpath(".//*div[@data-testid='cross-image']");
 
-
-
-
+    private By checkoutSummary = By.xpath("//*div[@data-testid='cart-pricing']");
+    private By totalMrpValue = By.xpath(".//*p[@data-testid='total-mrp']/span[@data-testid='mrp-value']");
+    private By discountOnMRPValue = By.xpath(".//*p[@data-testid='total-discount']/span[@data-testid='discount-value']");
+    private By taxValue = By.xpath(".//*p[@data-testid='total-tax']/span[@data-testid='tax-value']");
+    private By shippingChargesValue = By.xpath(".//*p[@data-testid='total-shipping-charges']/span[@data-testid='shipping-value']");
+    private By totalAmountValue = By.xpath(".//*p[@data-testid='final-amount']/span[@data-testid='final-amount-value']");
+    private By proceedToCheckoutButton = By.xpath(".//*div[@data-testid='cart-pricing']//button");
+    private By backToStoreButton = By.xpath("//a[@href='/shop']/button[contains(text(),'Back to Store')]");
+    private By clearCartButton = By.xpath("//button[contains(text(),'Clear Cart')]");
 
 
 }
