75962515e0b441be9fd752e742a79a5ed507e9fc
kamaljeet8130 <kamaljeet8130@gmail.com>
2026-08-18 23:21:16 +0530

    Inventory and product catalog project setup

---
+++ src/main/java/inventoryproductcatalog/InventoryService.java
@@ -0,0 +1,10 @@
+package inventoryproductcatalog;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InventoryService {
+    private Map<Integer,Product> products = new HashMap<>();
+
+
+}
---
+++ src/main/java/inventoryproductcatalog/Product.java
@@ -0,0 +1,49 @@
+package inventoryproductcatalog;
+
+public class Product {
+
+    private int id;
+    private String name;
+    private String category;
+    private double price;
+
+    public Product(int id, String name, String category, double price) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return id + " | " + name + " | " + category + " | ₹" + price;
+    }
+}
