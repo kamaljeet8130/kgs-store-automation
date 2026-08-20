bbca26e1216d3c1615991f92eb713af70ad83829
kamaljeet8130 <kamaljeet8130@gmail.com>
2026-08-20 23:32:07 +0530

    implement inventory product CRUD operations

--- src/main/java/inventoryproductcatalog/InventoryService.java
+++ src/main/java/inventoryproductcatalog/InventoryService.java
@@ -6,5 +6,49 @@ import java.util.Map;
 public class InventoryService {
     private Map<Integer,Product> products = new HashMap<>();
 
+    public boolean addProduct(Product product){
+        if(products.containsKey(product.getId())){
+            return false;
+        }
+        products.put(product.getId(),product);
+        return true;
+    }
+    public Product findProductById(int productId){
+        return products.get(productId);
+    }
+    public void displayProducts(){
+        if(products.isEmpty()){
+            System.out.println("No Products found");
+            return;
+        }
+        for(Map.Entry<Integer,Product> entry : products.entrySet()){
+            System.out.println(
+                    entry.getKey() + " -> "+ entry.getValue()
+            );
+        }
+    }
+    public boolean removeProduct(int productId){
+        if(!products.containsKey(productId)){
+            return false;
+        }
+        products.remove(productId);
+        return true;
+    }
+    public boolean updateProduct(
+            int productId,
+            String name,
+            String category,
+            double price
+    ){
+        Product product = products.get(productId);
+        if(product==null)
+            return false;
+
+        product.setName(name);
+        product.setCategory(category);
+        product.setPrice(price);
+
+        return true;
+    }
 
 }
